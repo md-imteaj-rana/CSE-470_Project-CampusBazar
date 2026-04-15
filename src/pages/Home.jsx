8bcfa11b1937e5c1871c1722a2688e9c5039b724
MD. Imteaj Rana <imteaj.rana2@gmail.com>
2026-04-15 15:39:50 +0600

    4th

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Newsletter from '../components/Newsletter'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
-      
+      <Hero></Hero>
+
+      <Newsletter></Newsletter>
     </div>
   )
 }
