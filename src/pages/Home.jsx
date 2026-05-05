33f82fec67a36da93c53e0f5c50d4a41ae153f5f
MD. Imteaj Rana <imteaj.rana2@gmail.com>
2026-05-05 17:04:04 +0600

    wishlist and home 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Newsletter from '../components/Newsletter'
 import Hero from '../components/Hero'
+import Categories from '../components/Categories'
+import HowItWorks from '../components/HowItWorks'
+import CTA from '../components/CTA'
 
 const Home = () => {
   return (
     <div>
         <title>Home</title>
       <Hero></Hero>
-
+      <Categories></Categories>
+      <HowItWorks></HowItWorks>
+      <CTA></CTA>
       <Newsletter></Newsletter>
     </div>
   )
